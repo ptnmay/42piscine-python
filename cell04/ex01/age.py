88426393ab0d28d6c8f7980e0969ba618ab0468c
ptnmay <pittinansaeyang@gmail.com>
2024-02-06 23:40:25 +0700

    cell04ex01

---
+++ cell04/ex01/age.py
@@ -0,0 +1,18 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    age.py                                             :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/02/06 23:34:25 by psaeyang          #+#    #+#              #
+#    Updated: 2024/02/06 23:39:28 by psaeyang         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+age = int(input("Please tell me your age: "))
+
+print(f"You are currently {age} years old.")
+print(f"In 10 years, you'll be {age + 10} years old.")
+print(f"In 20 years, you'll be {age + 20} years old.")
+print(f"In 30 years, you'll be {age + 30} years old.")
