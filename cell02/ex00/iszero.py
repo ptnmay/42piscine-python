187b77124b7aa74051d55d1a27c7e21b17d4a6fc
ptnmay <pittinansaeyang@gmail.com>
2024-02-06 21:50:07 +0700

    cell02ex00

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,18 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    iszero.py                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/02/06 21:38:02 by psaeyang          #+#    #+#              #
+#    Updated: 2024/02/06 21:45:06 by psaeyang         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+enter = int(input())
+
+if enter == 0:
+	print("This number is equal to zero.")
+else:
+	print("This number is different from zero.")
