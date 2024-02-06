cbf461974d3008b757e592568b59cb06537b8a72
ptnmay <pittinansaeyang@gmail.com>
2024-02-06 21:33:42 +0700

    cell01-ex02

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,16 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    whatsyourname.py                                   :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/02/06 21:29:01 by psaeyang          #+#    #+#              #
+#    Updated: 2024/02/06 21:33:15 by psaeyang         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+first_name = input("Hey, what's your first name? : ")
+last_name = input("And your last name? : ")
+
+print("Well, pleased to meet you, " + first_name + ' ' + last_name)
