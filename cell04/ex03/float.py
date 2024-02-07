3924585c6767b93ad20006a3380ac931f6caa182
ptnmay <pittinansaeyang@gmail.com>
2024-02-07 14:00:35 +0700

    cell04ex03

--- cell04/ex03/float.py
+++ cell04/ex03/float.py
@@ -6,9 +6,14 @@
 #    By: psaeyang <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/02/06 23:45:08 by psaeyang          #+#    #+#              #
-#    Updated: 2024/02/07 00:18:30 by psaeyang         ###   ########.fr        #
+#    Updated: 2024/02/07 13:58:52 by psaeyang         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+from decimal import Decimal
 
-num = input("Give me a number: ")
+num = float(input("Give me a number: "))
+if Decimal(num) % 1 == 0:
+	print("This number is an integer.")
+else:
+	print("This number is a decimal.")
