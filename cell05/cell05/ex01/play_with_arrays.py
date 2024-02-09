de6c1269d9176ec94ba8f205b6a582026eb4670d
Pittinan <105956506+ptnmay@users.noreply.github.com>
2024-02-10 01:02:03 +0700

    cell05

--- cell05/cell05/ex01/play_with_arrays.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-ori =  [2, 8, 9, 48, 8, 22, -12, 2]
-new = [i + 2 for i in ori]
-
-print(f"Original array: {ori}")
-print(f"New array: {new}")
--- cell05/cell05/ex04/parameters.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-import sys
-
-len = len(sys.argv)
-print("Number of parameters:", len - 1)
