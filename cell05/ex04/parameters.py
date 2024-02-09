a7e2cdb680ef3709ac6c31f69b8d1a05c778dd3f
ptnmay <pittinansaeyang@gmail.com>
2024-02-09 20:40:10 +0700

    cell05ex04-5

---
+++ cell05/ex04/parameters.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import sys
+
+len = len(sys.argv)
+print("Number of parameters:", len - 1)
---
+++ cell05/ex05/aff_first_param.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import sys
+
+len = len(sys.argv)
+
+if len > 1:
+	print(sys.argv[1])
+else:
+	print("none")
